ab63c3aa1366fa3a2a08cc7bac28078648bb252c
carson-troutman <carson.troutman@gmail.com>
2024-11-05 15:22:46 -0500

    read in first real estate dataset and began cleaning/filtering it

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Import packages
+import pandas as pd
+
+# Read in real estate data
+real_estate_df = pd.read_csv("data/realtor-data.zip.csv")
+
+# Take a look at real estate data - It may be best to run one line at a time
+# print(real_estate_df.describe())
+# print(real_estate_df.head())
+# print(real_estate_df.columns)
+
+# Clean real estate data
+
+#Select the rows for the state you want to sell roofs in, personally I'm in KY
+real_estate_df = real_estate_df[real_estate_df["state"] == "Kentucky"]
+
+# Take a look at the filtered dataset to make sure it worked
+# print(real_estate_df.head())
+
+# Get the number of rows you're working with
+print(len(real_estate_df)) #Kentucky rows = 26,316
--- set_up.py
+++ set_up.py
@@ -1,28 +1,21 @@
-#Import Packages
-import requests
-
 #API Calls
 
-# Create API call function
-def fetch_data_from_api(api_endpoint, params):
-    try:
-        response = requests.get(api_endpoint, params=params)
-    
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f"API request failed with status code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        raise Exception(f"An error occurred: {e}")
-    
+#Import Packages
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("ahmedshahriarsakib/usa-real-estate-dataset")
+
+# Call Real Estate API from Kaggle
+print("Path to dataset files:", path)
+
 #Call National Weather Service API - "https://api.weather.gov/"
 
-api_endpoint = "https://api.weather.gov/"
-params = {}
-data = fetch_data_from_api(api_endpoint, params)
-print(data)
+# api_endpoint = "https://api.weather.gov/"
+# params = {}
+# data = fetch_data_from_api(api_endpoint, params)
+# print(data)
 
-# Call Real Estate API - 
 # Here is a link with a list to choose from: https://gist.github.com/patpohler/36c731113fd113418c0806f62cbb9e30
 
 
